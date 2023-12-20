394d7aaa95fa0f4d4c90f7ad1c4e7378103d5c13
lancerace <lancerace144@gmail.com>
2023-12-20 21:39:43 +0800

    up

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,57 @@
 import './config';
-import * as express from "express";
+import express from 'express';
+import bodyParser from 'body-parser';
+import http from 'http';
+import cors from 'cors';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { schema } from './schema';
+import { AppDataSource } from './data-source';
 
 const app = express();
-app.listen(process.env.PORT || 8080, async () => {
-  console.log(`Server listening on port ${8080}`);
+const httpServer = http.createServer(app);
+
+
+
+const server = new ApolloServer({
+  schema: schema,
+  introspection: true,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+
 });
 
-//error handling middleware
-app.use(function (err, req, res, next) {
-  console.log("hi");
-  res.status(500).send('Something broke!');
-});
+AppDataSource.initialize().then(async () => {
+  console.log("Postgres TypeORM Database initialized");
+}).catch(error => console.log(error));
+
+(async () => {
+  await server.start();
+  app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    bodyParser.json(),
+    expressMiddleware(server, {
+      context: async () => ({ dataSource: AppDataSource })
+    })
+  );
+})();
+
+
+const corsOptions: cors.CorsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
+
+const port = Number.parseInt(process.env.PORT) || 8000;
+
+new Promise<void>((resolve) => httpServer.listen({ port: port }, resolve));
+console.log(`🚀 Server listening at: ${port}`);
+
+
+
 
 
 
