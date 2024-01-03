60609c11bb2dd5ad49e1e1768ab161b3b1067222
Kio Nguyen <vanhung.nguyen-ext@witco.io>
2024-01-03 20:46:05 +0700

    feat (back): handle create vehicle

--- backend/src/containers.ts
+++ backend/src/containers.ts
@@ -3,11 +3,14 @@ import { DBDataSource } from './database/datasource';
 import { Vehicle } from './database/entities/vehicle';
 import { IVehicleRepository } from './database/repositories/i-vehicle.repository';
 import { VehicleRepository } from './database/repositories/vehicle.repository';
+import { CreateVehicleUseCase } from './domain/usecases/vehicle/create-vehicle.usecase';
 import { GetVehiclesUsecase } from './domain/usecases/vehicle/get-vehicles.usecase';
+import { ICreateVehicleUseCase } from './domain/usecases/vehicle/i-create-vehicle.usecase';
 import { IGetVehiclesUseCase } from './domain/usecases/vehicle/i-get-vehicles.usecase';
 
 Container.set({ id: 'VEHICLE_REPOSITORY_SOURCE', factory: () => DBDataSource.getRepository(Vehicle) });
 Container.set({ id: IVehicleRepository, factory: () => new VehicleRepository(Container.get('VEHICLE_REPOSITORY_SOURCE')) });
 Container.set({ id: IGetVehiclesUseCase, factory: () => new GetVehiclesUsecase(Container.get(IVehicleRepository)) });
+Container.set({ id: ICreateVehicleUseCase, factory: () => new CreateVehicleUseCase(Container.get(IVehicleRepository)) });
 
 export default Container;
--- backend/src/presentation/verhicle.resolver.ts
+++ backend/src/presentation/verhicle.resolver.ts
@@ -1,9 +1,11 @@
 import 'reflect-metadata';
-import { Arg, Query, Resolver } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 import { Inject, Service } from 'typedi';
 import { Vehicle } from '../database/entities/vehicle';
+import { CreateVehicleInput } from '../domain/dto/create-vehicle-input.dto';
 import { PaginationArgument } from '../domain/dto/pagination-argument.dto';
 import { GetVehiclesResponse } from '../domain/types/get-vehicle-response.type';
+import { ICreateVehicleUseCase } from '../domain/usecases/vehicle/i-create-vehicle.usecase';
 import { IGetVehiclesUseCase } from './../domain/usecases/vehicle/i-get-vehicles.usecase';
 
 @Service()
@@ -12,7 +14,9 @@ export class VehicleResolver {
   constructor(
     // Inject service
     @Inject(IGetVehiclesUseCase)
-    private readonly getVehiclesUsecase: IGetVehiclesUseCase
+    private readonly getVehiclesUsecase: IGetVehiclesUseCase,
+    @Inject(ICreateVehicleUseCase)
+    private readonly createVehicleUseCase: ICreateVehicleUseCase
   ) {}
 
   @Query(() => GetVehiclesResponse, { name: 'getListVehicle' })
@@ -22,5 +26,10 @@ export class VehicleResolver {
     } catch (error) {
       console.log('🚀 ~ file: verhicle.resolver.ts:21 ~ VehicleResolver ~ getListVehicle ~ error:', error);
     }
+  }
+
+  @Mutation(_returns => Vehicle)
+  async createVehicle(@Arg('vehicle') vehicle: CreateVehicleInput): Promise<Vehicle> {
+    return await this.createVehicleUseCase.execute(vehicle);
   }
 }
